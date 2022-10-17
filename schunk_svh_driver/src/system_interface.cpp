5c6f3772334801dfca9294e18a43a798a4fce9e8
Stefan Scherzinger <scherzin@fzi.de>
2022-10-17 11:07:18 +0200

    Add license notice to all development files
    The text is in accordance with the recommendations from
    [here](https://www.gnu.org/licenses/gpl-howto.html)
    in the section *The license notices*.

--- schunk_svh_driver/src/system_interface.cpp
+++ schunk_svh_driver/src/system_interface.cpp
@@ -1,17 +1,24 @@
 ////////////////////////////////////////////////////////////////////////////////
-// Copyright 2022 FZI Research Center for Information Technology
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// © Copyright 2022 SCHUNK Mobile Greifsysteme GmbH, Lauffen/Neckar Germany
+// © Copyright 2022 FZI Forschungszentrum Informatik, Karlsruhe, Germany
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+// This file is part of the Schunk SVH Driver.
 //
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// The Schunk SVH Driver is free software: you can redistribute it and/or
+// modify it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or (at your
+// option) any later version.
+//
+// The Schunk SVH Driver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+// Public License for more details.
+//
+// You should have received a copy of the GNU General Public License along with
+// the Schunk SVH Driver. If not, see <https://www.gnu.org/licenses/>.
+//
+////////////////////////////////////////////////////////////////////////////////
 
 //-----------------------------------------------------------------------------
 /*!\file    system_interface.cpp
