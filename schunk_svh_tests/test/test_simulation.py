0c721149b1bc8aedf7f9e6aee197f1d4d8da0ed8
Stefan Scherzinger <stefan.scherzinger@de.schunk.com>
2024-10-02 18:20:03 +0200

    Add debug info for failing CI pipelines

--- schunk_svh_tests/test/test_simulation.py
+++ schunk_svh_tests/test/test_simulation.py
@@ -7,9 +7,13 @@ from test.conftest import launch_simulator
 
 @pytest.mark.launch(fixture=launch_simulator)
 def test_simulator_startup_works(launch_context, isolated):
+    print("#################  Starting a ros2 node")
     node = Node("test_simulator")
+    print("#################  Started the node")
     until_ready = 2.0  # sec
     time.sleep(until_ready)
+    print("#################  Getting node names")
     nodes = node.get_node_names()
+    print("#################  Got node names")
     print(nodes)
     assert "svh" in nodes
